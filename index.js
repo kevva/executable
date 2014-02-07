bd1983901bc03644e732fc43608f9453a3334a15
Kevin Martensson <kevinmartensson@gmail.com>
2014-02-07 09:34:34 +0100

    Use decimal mode to decide if a file is executable

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 var fs = require('fs');
 
 /**
- * Check if a file is executable
+ * Check if file is executable
+ *
+ * @param {String} mode
+ * @param {String} gid
+ * @param {String} uid
+ * @api private
+ */
+
+function isExe(mode, gid, uid) {
+    var ret;
+
+    if (process.platform === 'win32') {
+        ret = mode.toString(8) === '100755';
+    } else {
+        ret = (mode & '0001') ||
+              (mode & '0010') && process.getgid && gid === process.getgid() ||
+              (mode & '0100') && process.getuid && uid === process.getuid();
+    }
+
+    return ret;
+}
+
+/**
+ * Async
  *
  * @param {String} name
  * @param {Function} cb
@@ -16,7 +39,7 @@ module.exports = function (name, cb) {
             return cb(err);
         }
 
-        if (stats && stats.isFile() && stats.mode.toString(8) === '100755') {
+        if (stats && stats.isFile() && isExe(stats.mode, stats.gid, stats.gid)) {
             return cb(null, true);
         }
 
@@ -25,7 +48,7 @@ module.exports = function (name, cb) {
 };
 
 /**
- * Sync version
+ * Sync
  *
  * @param {String} name
  * @api public
@@ -34,7 +57,7 @@ module.exports = function (name, cb) {
 module.exports.sync = function (name) {
     var file = fs.statSync(name);
 
-    if (file && file.isFile() && file.mode.toString(8) === '100755') {
+    if (file && file.isFile() && isExe(file.mode, file.gid, file.uid)) {
         return true;
     }
 
